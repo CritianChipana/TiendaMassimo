ef5d9b1297ed8e0fa0ead2535bbbe450c03f7f10
CritianChipana <cristianchipanahuaman@gmail.com>
2021-01-29 10:08:39 -0500

    base de datos 2.0

--- Controlador/conexion.php
+++ Controlador/conexion.php
@@ -3,7 +3,8 @@ class conexion
 {
 	protected function conectar()
 	{
-		$a =mysqli_connect('localhost','root','admin','restaurante');
+		// $a =mysqli_connect('localhost','root','12345678','resta-massimo');
+		$a =mysqli_connect('localhost','root','12345678','restaurante');
 		// mysqli_select_db('sistema');
 		return $a;
 	}
